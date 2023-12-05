2fac1e374578d0c4fccec6e07d7abdff08b557af
thenewkgb <74326914+thenewkgb@users.noreply.github.com>
2023-12-05 06:38:44 +0000

    License added and WIP

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,48 @@
+/***********
+
+Copyright 2023 Kevin "thenewkgb" Barnard
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   
+*************/
+   
+   
 /*
 name: Infinite Line
+description: number puzzle containing four
+values in the centre and the directional
+nurmeric difference is added on top
 author: Kevin Barnard
 
-My next attempt at IL will be to separate
-the two halfs of the screen. Grid input and the keypad with differing TOUCHUP and
-TOUCHDOWN events. Both grid and keypad
+This attempt at IL will be to separate
+the two halfs of the screen. Grid input and the keypad. Both grid and keypad
 work together with the text input in the middle.
 
 also I wanted portrait and landscape modes.
+
 in this version the keypad will always be
-towards the top of the phone in landscape
+towards the top of the phone in
+either landscape mode
 
 this version can handle a keypress
 and moving a finger due with
 different TOUCHDOWN and TOUCHUP
 positions
+
+work in progress
+only portrait mode works
+and resolutions other than 720 width
+will cause problems
 */
 
 #include "game.hpp"
